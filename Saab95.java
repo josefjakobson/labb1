f135dc45c7c9e0e1cf75c53cb657efc1055051e5
MorrisIT <jonathan.svante@gmail.com>
2021-11-08 16:11:50 +0100

    Andra commiten

--- Saab95.java
+++ Saab95.java
@@ -3,11 +3,6 @@ import java.awt.*;
 public class Saab95 extends Car{
 
     public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
     
     public Saab95(){super(2,125,1,Color.red, "Saab95");
         stopEngine();
