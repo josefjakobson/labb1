04edc7222f948a06f61fc7743dcd1359bc778c8b
josefjakobson <j.jakobson28@gmail.com>
2021-11-10 10:56:20 +0100

    Main File Added

--- Labb1/Main.java
+++ Labb1/Main.java
@@ -1,7 +1,26 @@
 package Labb1;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
 
+        ArrayList<ArrayList<String>> grid = createGrid();
+
+
+    }
+
+    private static ArrayList<ArrayList<String>> createGrid() {
+        ArrayList<ArrayList<String>> grid = new ArrayList<>();
+
+        for (int i = 1; i <= 8; i++){
+            ArrayList<String> row = new ArrayList<>();
+            for (int j = 1; j <= 8; j++){
+                row.add(" ");
+            }
+            grid.add(row);
+        }
+
+        return grid;
     }
 }
