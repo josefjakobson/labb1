51e86344bcd8f8a99e4b581ec890e6599ab09478
MorrisIT <jonathan.svante@gmail.com>
2021-11-15 15:09:59 +0100

    Några tester

---
+++ src/TestCar/TestSaab95.java
@@ -0,0 +1,18 @@
+package TestCar;
+import java.awt.*;
+import Labb1.Saab95;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSaab95 {
+
+        @Test
+        public void test_Speedf() {
+            Saab95 Saab = new Saab95();
+            Saab.setTurboOn();
+            assertEquals(Saab.getSpeedFactor(), 1.625);
+        }
+
+
+
+    }
---
+++ src/TestCar/TestVolvo.java
@@ -0,0 +1,15 @@
+package TestCar;
+import Labb1.Saab95;
+import Labb1.Volvo240;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestVolvo {
+    @Test
+    public void test_Speedf(){
+        Volvo240 volvo = new Volvo240();
+        assertEquals(volvo.getSpeedFactor(),1.25);
+    }
+
+
+}
